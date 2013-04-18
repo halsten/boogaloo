07d8d71c0e1c981346eb608b10470acaf05b5c9b
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2013-04-18 13:18:52 +0200

    Eliminating logical variables at the end of an execution.

---
+++ Language/Boogie/Environment.hs
@@ -0,0 +1,288 @@
+{-# LANGUAGE TemplateHaskell, Rank2Types, FlexibleInstances, TypeSynonymInstances #-}
+
+-- | Execution state for the interpreter
+module Language.Boogie.Environment ( 
+  -- deepDeref,
+  Store,
+  emptyStore,
+  userStore,
+  storeDoc,
+  MapInstance,
+  MapCache,
+  Memory,
+  StoreLens,
+  memLocals,
+  memGlobals,
+  memOld,
+  memModified,
+  memConstants,
+  memMaps,
+  memLogical,
+  emptyMemory,
+  visibleVariables,
+  userMemory,
+  memoryDoc,
+  NameConstraints,
+  nameConstraintsDoc,
+  MapConstraints,
+  mapConstraintsDoc,
+  constraintUnion,  
+  ConstraintMemory,
+  conLocals,
+  conGlobals,
+  conMaps,
+  conLogical,
+  Environment,
+  envMemory,
+  envProcedures,
+  envConstraints,
+  envTypeContext,
+  envSolver,
+  envCustomCount,
+  envMapCount,
+  envLogicalCount,
+  envInOld,
+  initEnv,
+  lookupProcedure,
+  lookupNameConstraints,
+  lookupMapConstraints,
+  lookupCustomCount,
+  addProcedureImpl,
+  addNameConstraint,
+  addMapConstraint,
+  addLogicalConstraint,
+  setCustomCount,
+  markModified
+) where
+
+import Language.Boogie.Util
+import Language.Boogie.Position
+import Language.Boogie.AST
+import Language.Boogie.Solver
+import Language.Boogie.TypeChecker (Context, ctxGlobals)
+import Language.Boogie.Pretty
+import Language.Boogie.PrettyAST
+import Data.List
+import Data.Map (Map, (!))
+import qualified Data.Map as M
+import Data.Set (Set)
+import qualified Data.Set as S
+import Control.Lens hiding (Context, at)
+  
+{- Store -}  
+
+-- | Store: stores variable values at runtime 
+type Store = Map Id Thunk
+
+-- | A store with no variables
+emptyStore :: Store
+emptyStore = M.empty
+
+-- | Pretty-printed store
+storeDoc vars = vsep $ map varDoc (M.toList vars)
+  where varDoc (id, val) = pretty id <+> text "=" <+> pretty val
+    
+-- | 'userStore' @heap store@ : @store@ with all reference values completely dereferenced given @heap@
+userStore :: MapCache -> Store -> Store
+userStore heap store = store-- M.map (deepDeref heap . fromLiteral) store    
+
+-- | Partial map instance
+type MapInstance = Map [Thunk] Thunk
+
+instance Pretty MapInstance where
+  pretty inst = let
+      keysDoc keys = ((if length keys > 1 then parens else id) . commaSep . map pretty) keys
+      itemDoc (keys, v) = keysDoc keys  <+> text "->" <+> pretty v
+    in brackets (commaSep (map itemDoc (M.toList inst)))
+    
+-- | MapCache: stores partial map instances    
+type MapCache = Map Ref MapInstance
+  
+emptyCache = M.empty  
+  
+instance Pretty MapCache where
+  pretty cache = let
+      entryDoc (ref, val) = pretty ref <+> text "->" <+> pretty val
+    in vsep $ map entryDoc (M.toList cache)
+
+{- Memory -}
+
+-- | Memory: stores thunks associated with names, map references and logical variables
+data Memory = Memory {
+  _memLocals :: Store,      -- ^ Local variable store
+  _memGlobals :: Store,     -- ^ Global variable store
+  _memOld :: Store,         -- ^ Old global variable store (in two-state contexts)
+  _memModified :: Set Id,   -- ^ Set of global variables, which have been modified since the beginning of the current procedure  
+  _memConstants :: Store,   -- ^ Constant store  
+  _memMaps :: MapCache,         -- ^ Partial instances of maps
+  _memLogical :: Solution   -- ^ Logical variable store
+} deriving Eq
+
+makeLenses ''Memory
+
+-- | Lens that selects a store from memory
+type StoreLens = SimpleLens Memory Store
+
+-- | Empty memory
+emptyMemory = Memory {
+  _memLocals = emptyStore,
+  _memGlobals = emptyStore,
+  _memOld = emptyStore,
+  _memModified = S.empty,
+  _memConstants = emptyStore,
+  _memMaps = emptyCache,
+  _memLogical = M.empty
+}
+
+-- | Visible values of all identifiers in a memory (locals shadow globals) 
+visibleVariables :: Memory -> Store
+visibleVariables mem = (mem^.memLocals) `M.union` (mem^.memGlobals) `M.union` (mem^.memConstants)
+
+-- | Clear cached values if maps that have guard-less definitions
+clearDefinedCache :: MapConstraints -> MapCache -> MapCache
+clearDefinedCache conMaps heap = heap --foldr (\r -> update r emptyMap) heap definedRefs -- ToDo: fix!
+  -- where
+    -- definedRefs = [r | (r, (defs, _)) <- M.toList conMaps, any (\def -> node (defGuard def) == tt) defs]
+
+-- | 'userStore' @conMem mem@ : @mem@ with all reference values completely dereferenced and cache of defined maps removed 
+userMemory :: ConstraintMemory -> Memory -> Memory
+userMemory conMem mem = let heap' = clearDefinedCache (_conMaps conMem) (mem^.memMaps)
+  in over memLocals (userStore heap') $
+     over memGlobals (userStore heap') $
+     over memOld (userStore heap') $
+     over memModified (const S.empty) $
+     over memConstants (userStore heap') $
+     over memMaps (const emptyCache)
+     mem
+
+-- | 'memoryDoc' @inNames outNames mem@ : pretty-printed @mem@ where
+-- locals in @inNames@ will be printed as input variables
+-- and locals in @outNames@ will be printed as output variables
+memoryDoc :: [Id] -> [Id] -> Memory -> Doc
+memoryDoc inNames outNames mem = vsep $ 
+  docNonEmpty ins (labeledStoreDoc "Ins") ++
+  docNonEmpty locals (labeledStoreDoc "Locals") ++
+  docNonEmpty outs (labeledStoreDoc "Outs") ++
+  docNonEmpty ((mem^.memGlobals) `M.union` (mem^.memConstants)) (labeledStoreDoc "Globals") ++
+  docNonEmpty (mem^.memOld) (labeledStoreDoc "Old globals") ++
+  docWhen (not (S.null $ mem^.memModified)) (text "Modified:" <+> commaSep (map text (S.toList $ mem^.memModified))) ++
+  docWhen (mem^.memMaps /= emptyCache) (text "Maps:" <+> align (pretty (mem^.memMaps))) ++
+  docNonEmpty (mem^.memLogical) (labeledStoreDoc "Logical")
+  where
+    allLocals = mem^.memLocals
+    ins = restrictDomain (S.fromList inNames) allLocals
+    outs = restrictDomain (S.fromList outNames) allLocals
+    locals = removeDomain (S.fromList $ inNames ++ outNames) allLocals
+    labeledStoreDoc label store = (text label <> text ":") <+> align (storeDoc store)
+    docWhen flag doc = if flag then [doc] else [] 
+    docNonEmpty m mDoc = docWhen (not (M.null m)) (mDoc m)
+    
+instance Pretty Memory where
+  pretty mem = memoryDoc [] [] mem
+  
+{- Constraint memory -}
+
+-- | Mapping from names to their constraints
+type NameConstraints = Map Id ConstraintSet
+
+-- | Pretty-printed variable constraints
+nameConstraintsDoc :: NameConstraints -> Doc
+nameConstraintsDoc vars = vsep $ map varDoc (M.toList vars)
+  where varDoc (name, cs) = nest 2 (text name <+> pretty cs)
+
+-- | Mapping from map references to their parametrized constraints
+type MapConstraints = Map Ref ConstraintSet
+
+mapConstraintsDoc heap = vsep $ map valDoc (M.toList heap)
+  where valDoc (r, cs) = nest 2 (refDoc r <+> pretty cs)
+  
+-- | Union of constraints (values at the same key are concatenated)
+constraintUnion s1 s2 = M.unionWith (++) s1 s2  
+
+-- | Constraint memory: stores constraints associated with names, map references and logical variables
+data ConstraintMemory = ConstraintMemory {
+  _conLocals :: NameConstraints,        -- ^ Local name constraints
+  _conGlobals :: NameConstraints,       -- ^ Global name constraints
+  _conMaps :: MapConstraints,           -- ^ Parametrized map constraints
+  _conLogical :: ConstraintSet          -- ^ Constraint on logical variables
+}
+
+makeLenses ''ConstraintMemory
+
+-- | Symbolic memory with no constraints
+emptyConstraintMemory = ConstraintMemory {
+  _conLocals = M.empty,
+  _conGlobals = M.empty,
+  _conMaps = M.empty,
+  _conLogical = []
+}
+
+constraintMemoryDoc :: ConstraintMemory -> Doc
+constraintMemoryDoc mem = vsep $ 
+  docNonEmpty (mem^.conLocals) (labeledStoreDoc "Local con") ++
+  docNonEmpty (mem^.conGlobals) (labeledStoreDoc "Global con") ++
+  docNonEmpty (mem^.conMaps) (\h -> text "Map con:" <+> align (mapConstraintsDoc h)) ++
+  docWhen (not $ null (mem^.conLogical)) (text "Logical con:" <+> align (vsep (map pretty (mem^.conLogical))))
+  where
+    labeledStoreDoc label store = (text label <> text ":") <+> align (nameConstraintsDoc store)
+    docWhen flag doc = if flag then [doc] else [] 
+    docNonEmpty m mDoc = docWhen (not (M.null m)) (mDoc m)
+    
+instance Pretty ConstraintMemory where
+  pretty = constraintMemoryDoc
+
+{- Environment -}
+  
+-- | Execution state
+data Environment m = Environment
+  {
+    _envMemory :: Memory,                   -- ^ Values
+    _envConstraints :: ConstraintMemory,    -- ^ Constraints
+    _envProcedures :: Map Id [PDef],        -- ^ Procedure implementations
+    _envTypeContext :: Context,             -- ^ Type context
+    _envSolver :: Solver m,                 -- ^ Constraint solver
+    _envMapCount :: Int,                    -- ^ Number of map references currently in use
+    _envLogicalCount :: Int,                -- ^ Number of logical varibles currently in use
+    _envCustomCount :: Map Type Int,        -- ^ For each user-defined type, number of distinct values of this type already generated
+    _envInOld :: Bool                       -- ^ Is an old expression currently being evaluated?
+  }
+  
+makeLenses ''Environment
+   
+-- | 'initEnv' @tc s@: Initial environment in a type context @tc@ with constraint solver @s@  
+initEnv tc s = Environment
+  {
+    _envMemory = emptyMemory,
+    _envConstraints = emptyConstraintMemory,
+    _envProcedures = M.empty,
+    _envTypeContext = tc,
+    _envSolver = s,
+    _envCustomCount = M.empty,
+    _envMapCount = 0,
+    _envLogicalCount = 0,
+    _envInOld = False
+  }
+  
+-- | 'lookupGetter' @getter def key env@ : lookup @key@ in a map accessible with @getter@ from @env@; if it does not occur return @def@
+lookupGetter getter def key env = case M.lookup key (env ^. getter) of
+  Nothing -> def
+  Just val -> val
+  
+combineGetters f g1 g2 = to $ \env -> (env ^. g1) `f` (env ^. g2)  
+  
+-- Environment queries  
+lookupProcedure = lookupGetter envProcedures []  
+lookupNameConstraints = lookupGetter (combineGetters M.union (envConstraints.conLocals) (envConstraints.conGlobals)) []
+lookupMapConstraints = lookupGetter (envConstraints.conMaps) []
+lookupCustomCount = lookupGetter envCustomCount 0
+
+-- Environment modifications  
+addProcedureImpl name def env = over envProcedures (M.insert name (lookupProcedure name env ++ [def])) env
+addNameConstraint :: Id -> SimpleLens (Environment m) NameConstraints -> Expression -> Environment m -> Environment m
+addNameConstraint name lens c env = over lens (M.insert name (nub $ c : lookupGetter lens [] name env)) env
+addMapConstraint r c env = over (envConstraints.conMaps) (M.insert r (nub $ c : lookupMapConstraints r env)) env
+addLogicalConstraint c = over (envConstraints.conLogical) (nub . (c :))
+setCustomCount t n = over envCustomCount (M.insert t n)
+markModified name env = if M.member name (env^.envTypeContext.to ctxGlobals) 
+  then over (envMemory.memModified) (S.insert name) env
+  else env
---
+++ Language/Boogie/Z3/Solution.hs
@@ -0,0 +1,125 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE BangPatterns #-}
+module Language.Boogie.Z3.Solution 
+    ( solveConstr
+    , extract
+    ) where
+
+import           Control.Applicative
+import           Control.Lens ((%=), uses)
+import           Control.Monad
+
+import           Data.Generics (everything, mkQ, gmapQ)
+import qualified Data.Set as Set
+import           Data.Set (Set)
+import qualified Data.Map as Map
+import           Data.Map (Map)
+
+import           Z3.Monad
+
+import           Language.Boogie.AST
+import           Language.Boogie.Position
+import           Language.Boogie.PrettyAST ()
+import           Language.Boogie.Solver
+import           Language.Boogie.Z3.Eval
+import           Language.Boogie.Z3.GenMonad
+
+-- | Update the state's reference map with the references in the
+-- supplied expressions. This requires that the sorts already be
+-- in place in the state.
+updateRefMap :: [Expression] -> Z3Gen ()
+updateRefMap = mapM_ addRefs
+    where
+      addRefs :: Expression -> Z3Gen ()
+      addRefs e =
+          do let rs = refs e
+             pairs <- mapM (\r -> (r,) <$> declareRef r) (Set.toList rs)
+             let updMap = Map.fromList pairs
+             refMap %= Map.union updMap
+
+      -- | Get the values from a single expression.
+      refs :: Expression -> Set TaggedRef
+      refs = everything Set.union (mkQ Set.empty go)
+          where
+            go (Logical t ref) = Set.singleton (LogicRef t ref)
+            go e               = Set.unions (gmapQ (mkQ Set.empty go) e)
+
+      refStr :: TaggedRef -> String
+      refStr (LogicRef _ r) = "logical_" ++ show r
+
+      refType :: TaggedRef -> Type
+      refType (LogicRef t _) = t
+
+      declareRef :: TaggedRef -> Z3Gen AST
+      declareRef tRef =
+          do symbol <- mkStringSymbol (refStr tRef)
+             sort   <- lookupSort (refType tRef)
+             mkConst symbol sort
+
+-- | Given a set of constraint expressions produce a mapping
+-- of references to their concrete values.
+--
+-- The constraint expressions will have no regular variables,
+-- only logical variables and map variables.
+
+solveConstr :: [Expression] -> Z3Gen (Maybe Solution)
+solveConstr constrs = 
+    do updateRefMap constrs
+       mapM_ (evalExpr >=> assertCnstr) constrs
+       (_result, modelMb) <- getModel
+       case modelMb of
+         Just model -> Just <$> reconstruct model
+         Nothing -> return Nothing
+
+
+-- | Extracts a particular type from an AST node, evaluating
+-- the node first.
+extract :: Model -> Type -> AST -> Z3Gen Value
+extract model t ast = 
+    do Just ast' <- eval model ast
+       case t of 
+         IntType -> IntValue <$> getInt ast'
+         BoolType -> 
+             do bMb <- getBool ast'
+                case bMb of
+                  Just b -> return $ BoolValue b
+                  Nothing -> error "solveConstr.reconstruct.extract: not bool"
+         IdType ident types ->
+             do proj <- lookupCustomProj ident types
+                extr <- mkApp proj [ast']
+                Just evald <- eval model extr
+                int <- getInt evald
+                return (CustomValue t $ fromIntegral int)
+         _ ->
+             error $ concat [ "solveConstr.reconstruct.extract: can't "
+                            , "extract maptypes like "
+                            , show t
+                            ]
+
+-- | From a model and a mapping of values to Z3 AST nodes reconstruct
+-- a mapping from references to values. This extracts the appropriate
+-- values from the model.
+reconstruct :: Model -> Z3Gen Solution
+reconstruct model =
+    do logicMap <- reconMaps
+       return (Map.map (gen . Literal) logicMap)
+    where
+      extract' = extract model
+
+      -- | Reconstruct all maps
+      reconMaps :: Z3Gen (Map Ref Value)
+      reconMaps = 
+          do refAssoc <- uses refMap Map.toList 
+             foldM go Map.empty refAssoc
+          where go m (tRef, ast) =
+                    do (r, v) <- reconLogicRef tRef ast
+                       return (Map.insert r v m)
+
+      -- | Reconstruct a ref/value pair for a logical reference.
+      reconLogicRef :: TaggedRef -> AST -> Z3Gen (Ref, Value)
+      reconLogicRef (LogicRef t ref) ast =
+          do Just ast' <- eval model ast
+             x <- extract' t ast'
+             return (ref, x)
